34c249863c9381ac70951bedacfc6a3afd3d4b9b
ico798 <ruben.nieto@ico.es>
2018-11-06 16:42:01 +0100

    dfd

--- PruebaJenkins/src/test/java/PruebaJenkins/PruebaJenkins/AppTest.java
+++ PruebaJenkins/src/test/java/PruebaJenkins/PruebaJenkins/AppTest.java
@@ -39,7 +39,7 @@ public class AppTest
 			//System.setProperty("webdriver.chrome.driver", "C:\\_ej_ico_0\\Selenium 22_10_2018\\chromedriver.exe");
 		  	
 		 //System.setProperty("webdriver.chrome.driver","chromedriver");
-		// System.setProperty("webdriver.chrome.driver","$JENKINS_HOME\chromedriver");
+		 System.setProperty("webdriver.chrome.driver","\\$JENKINS_HOME/chromedriver");
 		 
 		
 		 //headless chrome
